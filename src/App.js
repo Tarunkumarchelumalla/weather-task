b9f081618710a34b50da1f279b942a7c796b447e
TarunChelumalla <tarunchelumalla@gmail.com>
2023-08-30 21:29:12 +0530

    globe-initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Globe from 'react-globe.gl';
+import { Country, State, City }  from 'country-state-city';
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [places, setPlaces] = useState(Country.getAllCountries());
+
+  useEffect(() => {
+  
+
+      console.log(places)
+    
+
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='search-globe'>Hello</div>
+
+      <Globe
+    globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+    // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+ 
+    labelsData={places}
+    labelLat={d => d.latitude}
+    labelLng={d => d.longitude}
+    labelText={d => d.name}
+    labelSize={d => Math.sqrt (900 * 4e-4)}
+    labelDotRadius={d => Math.sqrt(400 * 4e-4)}
+    labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+    labelResolution={5}
+  />
     </div>
+
+
   );
 }
 
