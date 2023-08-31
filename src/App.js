8b49c7fe821bfe7377f3cf1d1c76fa86c3decd98
TarunChelumalla <tarunchelumalla@gmail.com>
2023-09-01 01:21:51 +0530

    complete Ui changes Marker, Weatehr Api integrated

--- src/App.js
+++ src/App.js
@@ -3,36 +3,142 @@ import './App.css';
 import Globe from 'react-globe.gl';
 import { Country, State, City }  from 'country-state-city';
 import { useEffect, useState } from 'react';
+import Weather from './weather';
+import ReactDOM from 'react-dom';
 function App() {
 
+  const [inputValue, setInputValue] = useState('');
   const [places, setPlaces] = useState(Country.getAllCountries());
+  const [copyPlaces, setCopyPlaces] = useState(Country.getAllCountries());
+  const [shouldRenderDiv, setShouldRenderDiv] = useState(false)
+  const [globeHtmlData,setGlobeHtmlData] = useState()
+  const [searchDoc, setSearchDoc] = useState();
+  const apiKey = process.env.API_Key;
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const globeData = (coordinates, event) =>{
+    console.log('Clicked coordinates:', coordinates);
+  }
+  const markerSvg =  () => (
+    <Weather WeatherDOc={searchDoc} />
+  );
+
+  const handleInputChange=(event)=>{
+
+    setShouldRenderDiv(true)
+    console.log(event.target.value)
+    setInputValue(event.target.value)
+    const filteredPlaces = copyPlaces.filter(place =>
+      place.name.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      setCopyPlaces(filteredPlaces)
+    if(event.target.value === ""){ setCopyPlaces(JSON.parse(JSON.stringify(places))); }
+  }
+
+  const handleSelectChange=(event)=>{
+    const selectedObject = copyPlaces.find(v=>v.phonecode===event.target.value)
+
+    setInputValue( selectedObject.name)
+    setSearchDoc(selectedObject)
+    setShouldRenderDiv(false)
+  
+  }
+
+  const handleLabelClick =(label, event, { lat, lng, altitude })=>{
+    console.log(label)
+  }
 
   useEffect(() => {
-  
 
       console.log(places)
-    
-
-
+  
   }, []);
+
+  const handleSearch=(e)=>{
+    e.preventDefault();
+    setGlobeHtmlData( [{
+      lat: searchDoc.latitude,
+      lng: searchDoc.longitude,
+      size: 30,
+      color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+    }])
+    console.log(searchDoc)
+    async function fetchWeatherData() {
+      try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${searchDoc.latitude}&lon=${searchDoc.longitude}&appid=${'335e6552f243a3ca8fef84eac232846e'}`);
+        
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+
+        console.log(data)
+        let el = document.getElementById('focus')
+
+        ReactDOM.render(<Weather WeatherDOc={data} />, el);
+        
+
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    }
+
+    fetchWeatherData();
+ 
+  }
 
   return (
     <div className='main-container'>
-      <div className='search-globe'>Hello</div>
+      <div className='input-box'>      
+        <form className='from-class'>   
+            <label  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input id="countries"  value={inputValue}
+                onChange={handleInputChange} type="search"  className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search country..." required/>
+                <button disabled={!inputValue.length} onClick={handleSearch} className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Search</button>
+            </div>
+            
+            {shouldRenderDiv &&<select id='select' size={5} onChange={handleSelectChange}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            { copyPlaces.map((curr,index)=>(<option key={index} value={curr.phonecode}>{curr.name}</option>))}
+            </select>}
+        </form>
+      
+      </div>
 
       <Globe
-    globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-    // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
- 
-    labelsData={places}
-    labelLat={d => d.latitude}
-    labelLng={d => d.longitude}
-    labelText={d => d.name}
-    labelSize={d => Math.sqrt (900 * 4e-4)}
-    labelDotRadius={d => Math.sqrt(400 * 4e-4)}
-    labelColor={() => 'rgba(255, 165, 0, 0.75)'}
-    labelResolution={5}
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      
+      onGlobeClick={globeData}
+      labelsData={places}
+      labelLat={d => d.latitude}
+      labelLng={d => d.longitude}
+      labelText={d => d.name}
+      labelSize={d => Math.sqrt (900 * 4e-4)}
+      labelDotRadius={d => Math.sqrt(400 * 4e-4)}
+      labelColor={() => 'rgba(255, 165, 0, 0.75)'}
+      labelResolution={2}
+      enablePointerInteraction={true}
+      htmlElementsData={globeHtmlData}
+      htmlElement={d => {
+        const el = document.createElement('div');
+        el.id= 'focus'
+        el.style.color = d.color;
+        el.style.width = `${d.size}px`;
+
+        el.style['pointer-events'] = 'auto';
+        el.style.cursor = 'pointer';
+        el.onclick = () => console.info(d);
+        return el;
+      }}
+      onLabelClick={handleLabelClick}
   />
+
     </div>
 
 
---
+++ src/weather.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import './App.css';
+function Weather(WeatherDOc) {
+    console.log(WeatherDOc)
+    const doc= WeatherDOc.WeatherDOc
+  return (
+    <div className='main-weather bg-blue-700'>
+
+        <div className='location-container'>
+        <div className="svg-container"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 20.8995L16.9497 15.9497C19.6834 13.2161 19.6834 8.78392 16.9497 6.05025C14.2161 3.31658 9.78392 3.31658 7.05025 6.05025C4.31658 8.78392 4.31658 13.2161 7.05025 15.9497L12 20.8995ZM12 23.7279L5.63604 17.364C2.12132 13.8492 2.12132 8.15076 5.63604 4.63604C9.15076 1.12132 14.8492 1.12132 18.364 4.63604C21.8787 8.15076 21.8787 13.8492 18.364 17.364L12 23.7279ZM12 13C13.1046 13 14 12.1046 14 11C14 9.89543 13.1046 9 12 9C10.8954 9 10 9.89543 10 11C10 12.1046 10.8954 13 12 13ZM12 15C9.79086 15 8 13.2091 8 11C8 8.79086 9.79086 7 12 7C14.2091 7 16 8.79086 16 11C16 13.2091 14.2091 15 12 15Z"></path></svg></div><span>{doc.name}</span>
+        </div>
+        <div className="main-weather-contianer">
+            <div className='temp-container'>
+                <p>{Math.floor(doc.main.temp-273)}</p><span>&#176;</span>C
+            </div>
+            <div className='w-[1px] bg-black'></div>
+            <div className='main-misc-container'>
+                <div className='wind-container text-[black]'>
+                    <div className=' bg-[#FFE4BE] data text-[black]'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10.5 17H4V15H10.5C12.433 15 14 16.567 14 18.5C14 20.433 12.433 22 10.5 22C8.99957 22 7.71966 21.0559 7.22196 19.7293L9.09513 19.0268C9.30843 19.5954 9.85696 20 10.5 20C11.3284 20 12 19.3284 12 18.5C12 17.6716 11.3284 17 10.5 17ZM5 11H18.5C20.433 11 22 12.567 22 14.5C22 16.433 20.433 18 18.5 18C16.9996 18 15.7197 17.0559 15.222 15.7293L17.0951 15.0268C17.3084 15.5954 17.857 16 18.5 16C19.3284 16 20 15.3284 20 14.5C20 13.6716 19.3284 13 18.5 13H5C3.34315 13 2 11.6569 2 10C2 8.34315 3.34315 7 5 7H13.5C14.3284 7 15 6.32843 15 5.5C15 4.67157 14.3284 4 13.5 4C12.857 4 12.3084 4.40463 12.0951 4.97317L10.222 4.27073C10.7197 2.94414 11.9996 2 13.5 2C15.433 2 17 3.567 17 5.5C17 7.433 15.433 9 13.5 9H5C4.44772 9 4 9.44772 4 10C4 10.5523 4.44772 11 5 11Z"></path></svg>
+                    </div>
+                    <p>{Math.floor(doc.wind.speed)}m/s</p>
+
+                </div>
+                <div className='wind-container text-[black]'>
+                    <div className=' bg-[#FFE4BE] data'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 3.09698L7.05025 8.04673C4.31658 10.7804 4.31658 15.2126 7.05025 17.9462C9.78392 20.6799 14.2161 20.6799 16.9497 17.9462C19.6834 15.2126 19.6834 10.7804 16.9497 8.04673L12 3.09698ZM12 0.268555L18.364 6.63252C21.8787 10.1472 21.8787 15.8457 18.364 19.3604C14.8492 22.8752 9.15076 22.8752 5.63604 19.3604C2.12132 15.8457 2.12132 10.1472 5.63604 6.63252L12 0.268555Z"></path></svg></div>
+                    <p>{Math.floor(doc.main.humidity)}%</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Weather
+
+// {
+//     "WeatherDOc": {
+//         "coord": {
+//             "lon": 19.9,
+//             "lat": 60.1167
+//         },
+//         "weather": [
+//             {
+//                 "id": 804,
+//                 "main": "Clouds",
+//                 "description": "overcast clouds",
+//                 "icon": "04n"
+//             }
+//         ],
+//         "base": "stations",
+//         "main": {
+//             "temp": 288.18,
+//             "feels_like": 288.19,
+//             "temp_min": 288.18,
+//             "temp_max": 288.71,
+//             "pressure": 1004,
+//             "humidity": 94
+//         },
+//         "visibility": 10000,
+//         "wind": {
+//             "speed": 2.06,
+//             "deg": 300
+//         },
+//         "clouds": {
+//             "all": 100
+//         },
+//         "dt": 1693508058,
+//         "sys": {
+//             "type": 1,
+//             "id": 1347,
+//             "country": "AX",
+//             "sunrise": 1693452705,
+//             "sunset": 1693504227
+//         },
+//         "timezone": 10800,
+//         "id": 3336453,
+//         "name": "Julle",
+//         "cod": 200
+//     }
+// }
